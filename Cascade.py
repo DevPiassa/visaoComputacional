59d960bfe9643c4a413ecdb2127ffbe421ab9dd9
AbelPiassa <abel.beleleu@gmail.com>
2025-05-21 10:19:45 -0300

    Update Cascade.py

--- Cascade.py
+++ Cascade.py
@@ -1,14 +1,14 @@
 import cv2
 
 video = cv2.VideoCapture()
-ip = "ip,https/video" # para abrir webcan do celular "app ipWebcam "
+ip = "https://192.168.15.153:8080/video" # para abrir webcan do celular "app ipWebcam "
 video.open(ip)
-classicador = cv2.CascadeClassifier(r'cascades\haarcascade_eye.xml')
+classicador = cv2.CascadeClassifier(r'cascades\haarcascade_frontalface_alt.xml')
 
 while True:
     check, img = video.read()
     imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    objetos = classicador.detectMultiScale(imgGray)
+    objetos = classicador.detectMultiScale(imgGray,minSize=(50,50),scaleFactor=1.5)
     for x,y,l,a in objetos:
         cv2.rectangle(img,(x,y),(x+l,y+a),(255,0,0),2)
 
